f5f115b8722948dd24731cab8c1331e37e9bd31a
Prince0297Ahirwar <princeahirwar51@gmail.com>
2021-06-01 23:33:38 +0530

    add authenticated demo key

--- index.js
+++ index.js
@@ -2,19 +2,27 @@
 
 function displayImage(data){
      //we can add html tag using jquery and then append to another container
-     $('<img>',{
-         src:data.url,
-         height:'100%',
-         width:'100%'
-     }).appendTo('#image-container');
+    //  $('<img>',{
+    //      src:data.url,
+    //      height:'100%',
+    //      width:'100%'
+    //  }).appendTo('#image-container');
+
+    var img = $(document.createElement('img'));
+    img.attr('src',data.url);
+    img.appendTo('#image-container');
 }
 
 
 $.ajax(
     {
-        url:'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2018-07-14',
+        url:'https://api.nasa.gov/planetary/apod',
         method:'GET',
-        success:displayImage
+        success:displayImage,
+        data:{
+            api_key:'HTdy91oO6D6EzEIb3yozi3AoIAXVbjtd8EVbzML8',
+            date:'2018-07-04'
+        }
     }
 );
 
